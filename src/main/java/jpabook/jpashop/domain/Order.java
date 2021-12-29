727359f6931e3349fd00b676b435e7e1b6de0e17
Charming (차밍) <loadkrnis@gmail.com>
2021-12-30 02:26:06 +0900

    feat: cancel, totalPrice logic in Order

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static javax.persistence.CascadeType.ALL;
@@ -40,12 +39,11 @@ public class Order {
     private OrderStatus status;
 
     // == 생성 메서드 == //
-    public static Order createOrder(Member member, Delivery delivery, LocalDateTime now, OrderItem... orderItems) {
+    public static Order createOrder(Member member, Delivery delivery, List<OrderItem> orderItems, LocalDateTime now) {
         Order order = new Order();
         order.setMember(member);
         order.setDelivery(delivery);
-        Arrays.stream(orderItems)
-                .forEach(order::addOrderItem);
+        orderItems.forEach(order::addOrderItem);
         order.setStatus(OrderStatus.ORDER);
         order.setOrderDate(now);
 
@@ -66,5 +64,25 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // == 비즈니스 로직 == //
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("배송이 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        orderItems.forEach(OrderItem::cancel);
+
+    }
+
+    // == 조회 로직 == //
+    public int getTotalPrice() {
+        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
     }
 }
