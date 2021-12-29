aaa0e67c091920c9509f522ca7ad3c44379e39d9
Charming (차밍) <loadkrnis@gmail.com>
2021-12-30 02:17:18 +0900

    feat: Order factory method

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static javax.persistence.CascadeType.ALL;
@@ -37,6 +38,19 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    // == 생성 메서드 == //
+    public static Order createOrder(Member member, Delivery delivery, LocalDateTime now, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        Arrays.stream(orderItems)
+                .forEach(order::addOrderItem);
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(now);
+
+        return order;
+    }
 
     // == 연관관계 메서드 == //
     public void setMember(Member member) {
