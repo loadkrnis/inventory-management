475265209833166cd31013f2f78fd6b131a4caf4
Charming (차밍) <loadkrnis@gmail.com>
2021-12-30 22:20:12 +0900

    ref: noArgsConstructor

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import static javax.persistence.FetchType.LAZY;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id
     @GeneratedValue
@@ -27,9 +30,6 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
-
-    protected OrderItem() {
-    }
 
     // == 펙토리 메서드 == //
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
